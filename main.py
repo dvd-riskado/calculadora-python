633e6a47194e8160dc185e38203980357b06b537
Neiva <nnivaxavier@gmail.com>
2026-07-04 16:26:37 -0300

    Calculadora simples completa

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello world")
+## Aqui é uma espécie de menu, mostrando o que fazer e perguntando a entrada do usuário
+print("== Calculadora ==")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - DIvisão")
+operacao = int(input("Escolha uma opção: "))
+
+## Aqui, o sistema pede os números que serão usados na operacao escolhida
+num1 = float(input("Primeiro número  -> "))
+num2 = float(input("Segundo número -> "))
+
+if(operacao == 1){
+    resultado = num1 + num2
+    print(f"{num1} + {num2} = {resultado}")
+}
+elif(operacao == 2){
+    resultado = num1 - num2
+    print(f"{num1} - {num2} = {resultado}")
+}
+elif(operacao == 3){
+    resultado = num1 * num2
+    print(f"{num1} x {num2} = {resultado}")
+}
+elif(operacao == 4){
+    resultado = num1 / num2
+    print(f"{num1} : {num2} = {resultado}")
+}
+else{
+    print("Operação inválida!")
+}
